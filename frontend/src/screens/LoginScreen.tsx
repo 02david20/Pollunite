029f7b8e28b8c9f97742ebd0dd761e005f19cb22
Quang Khánh <khanhnguyenmcu@gmail.com>
2023-03-18 17:59:12 +0700

    Sign in with firebase

--- frontend/src/screens/LoginScreen.tsx
+++ frontend/src/screens/LoginScreen.tsx
@@ -1,19 +1,24 @@
-import * as React from 'react'
+import { useState } from 'react'
 import {Text, View, Pressable} from 'react-native'
 import TextInputComponent from '../components/TextInputComponent';
 import PassWordComponent from '../components/PasswordComponent';
 import GoogleIcon from '../../assets/svg/google.svg'
 import FacebookIcon from '../../assets/svg/facebook.svg'
+import {signIn} from '../services/firebaseAuth';
+
 const LoginScreen = (): JSX.Element => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
     return (
         <View className="w-11/12 mx-auto h-full flex-col justify-center">
            <Text className="font-extrabold text-4xl text-center text-[#4CAF50]">Login</Text>
            <View className="mt-3">
             <View>
-                <TextInputComponent placeholder="Email" color="#4CAF50" />
-                <PassWordComponent placeholder="Password" color="#4CAF50" />
+                <TextInputComponent placeholder="Email" color="#4CAF50" value={email} setValue={(text: string) => setEmail(text)} />
+                <PassWordComponent placeholder="Password" color="#4CAF50" value={password} setValue={(text: string) => setPassword(text)} />
             </View>
-           <Pressable className="w-11/12 mx-auto mt-5">
+           <Pressable className="w-11/12 mx-auto mt-5" onPress={() => signIn(email, password)}>
                 <View className="bg-[#4CAF50] rounded-xl">
                     <Text className="text-white p-5 text-center not-italic font-semibold text-base leading-6">Log In</Text>
                 </View>
