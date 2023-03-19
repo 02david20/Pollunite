775e41f04713a3bbe0207be5c152e3d2df31c644
Kha Sang <sang.khask12@hcmut.edu.vn>
2023-03-19 10:28:26 +0700

    add ref marker

--- frontend/src/screens/Map/styles.tsx
+++ frontend/src/screens/Map/styles.tsx
@@ -4,16 +4,20 @@ var maxWidth = Dimensions.get('window').width //full width
 var maxHeight = Dimensions.get('window').height //full height
 
 const styles = StyleSheet.create({
-    container: {
-        width: 400,
-        height: maxHeight,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    map: {
-        ...StyleSheet.absoluteFillObject
-      },
-})
+  container: {
+    width: 400,
+    height: maxHeight,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  view: {
+    justifyContent: "flex-end",
+    margin: 0,
+  },
+});
 
 export default styles
